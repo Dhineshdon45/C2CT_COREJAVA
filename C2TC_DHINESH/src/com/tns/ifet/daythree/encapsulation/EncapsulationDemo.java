40546e1ddf8417718d2e9a09c6b4a009e40cfa90
ELCOT <ELCOT@Lenovo>
2024-11-11 14:23:08 +0530

    first commit

---
+++ C2TC_DHINESH/src/com/tns/ifet/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.daythree.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		//object creation
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(33);
+		obj.setName("Dhinesh");
+		obj.setAge(20);
+		System.out.println(obj);
+
+
+	}
+
+}
---
+++ C2TC_DHINESH/src/com/tns/ifet/daythree/scannerdemo/ScannerDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.daythree.scannerdemo;
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int a=sc.nextInt();
+		System.out.println("your entered nummber is "+a);
+
+	}
+
+}
